2898e73c75aa9b1b3bbacbab22e1b5c7a41f8005
Ariel Lagares <ariel.colonlagares@ey.com>
2016-05-13 12:49:56 -0400

    Image assets, site logo, make site fullwidth, table column highlight.

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,11 +1,15 @@
 $('th').click(function() {
     var th_index = $(this).index();
     $('tr').each(function() {
-        if($(this).find('td').eq(th_index).hasClass('highlight')){
-            $(this).find('td').eq(th_index).removeClass('highlight');
-        } else {
-            $(this).find('td').removeClass('highlight');
-            $(this).find('td').eq(th_index).toggleClass('highlight');
-        }
+    	$('th').removeClass('highlight');
+        $(this).find('td').removeClass('highlight');
+    	$(this).addClass('highlight');
+    	$(this).find('td').eq(th_index -1).addClass('highlight');
+        // if($(this).find('td').eq(th_index -1).hasClass('highlight')){
+        //     $(this).find('td').eq(th_index -1).removeClass('highlight');
+        // } else {
+        //     $(this).find('td').removeClass('highlight');
+        //     $(this).find('td').eq(th_index -1).toggleClass('highlight');
+        // }
     });
 });
