88e2684ffb62af229dcb9c528d2d4efaf1ba7fb9
Ariel Lagares <ariel.colonlagares@ey.com>
2016-05-13 15:10:22 -0400

    Table styling, refined column highlight.

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,9 +1,9 @@
 $('th').click(function() {
     var th_index = $(this).index();
+	$('thead th').removeClass('highlight');
+    $(this).addClass('highlight');
     $('tr').each(function() {
-    	$('th').removeClass('highlight');
         $(this).find('td').removeClass('highlight');
-    	$(this).addClass('highlight');
     	$(this).find('td').eq(th_index -1).addClass('highlight');
         // if($(this).find('td').eq(th_index -1).hasClass('highlight')){
         //     $(this).find('td').eq(th_index -1).removeClass('highlight');
