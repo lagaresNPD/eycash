79df5cb908d7a77774236acf184ff65394a854a7
Ariel Lagares <ariel.colonlagares@ey.com>
2016-06-02 11:36:54 -0400

    remove unused libraries, simulate Chips filter behavior.

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -2,62 +2,74 @@
 /* highlight table column */
 $('.highlightable-column th').click(function() {
 
-    var th_index = $(this).index();
+	var th_index = $(this).index();
 
-    $('.highlightable-column thead th').removeClass('highlight');
-    $('.highlightable-column td').removeClass('highlight');
+	$('.highlightable-column thead th').removeClass('highlight');
+	$('.highlightable-column td').removeClass('highlight');
 
    if($(this).index() !== 0) {
-        $(this).addClass('highlight');
-        $('tr').each(function() {
-            if($(this).find('td').eq(th_index -1).hasClass('highlight')){
-                $(this).find('th').eq(th_index -1).removeClass('highlight');
-                $(this).find('td').eq(th_index -1).removeClass('highlight');
-            } else {
-                $(this).find('td').removeClass('highlight');
-                // $(this).find('th').eq(th_index).addClass('highlight');
-                $(this).find('td').eq(th_index -1).toggleClass('highlight');
-            }
-        });
-    }
+		$(this).addClass('highlight');
+		$('tr').each(function() {
+			if($(this).find('td').eq(th_index -1).hasClass('highlight')){
+				$(this).find('th').eq(th_index -1).removeClass('highlight');
+				$(this).find('td').eq(th_index -1).removeClass('highlight');
+			} else {
+				$(this).find('td').removeClass('highlight');
+				// $(this).find('th').eq(th_index).addClass('highlight');
+				$(this).find('td').eq(th_index -1).toggleClass('highlight');
+			}
+		});
+	}
 });
 
 /* set filters label to name of selected link */
 $('.filters .btn-group a').click( function () {
-    var dd_result = $(this).text();
-    var dd_id = $(this).parent().parent().attr('data-id');
-    // var dd_id = $(this).closest('button span[id]').attr('id');
-    // console.log($(dd_id));
+	var dd_result = $(this).text();
+	var dd_id = $(this).parent().parent().attr('data-id');
+	// var dd_id = $(this).closest('button span[id]').attr('id');
+	// console.log($(dd_id));
 
-    $('#' + dd_id).text(dd_result);
+	$('#' + dd_id).text(dd_result);
+});
+
+/* hides all but the first two options in the PPG dropdown whenever "Chips" is selected in Category */
+var category_opts = $('.filters .dropdown-menu[data-id=result2] li');
+var ppg_opts = $('.filters .dropdown-menu[data-id=result5] li');
+$(category_opts).click( function () {
+	$(ppg_opts).removeClass('hidden');
+});
+$(category_opts).eq(0).click( function () {
+	for (i=1; i < ppg_opts.length - 1; i++) {
+		$(ppg_opts).eq(i).addClass('hidden');
+	}
 });
 
 /* highlight date in mini calendar */
 $('.filters .mini-calendar tbody td').click( function () {
-    var calId = $(this).closest('table').attr('id');
-    $('#' + calId + ' td').removeClass('active');
-    $(this).toggleClass('active');
+	var calId = $(this).closest('table').attr('id');
+	$('#' + calId + ' td').removeClass('active');
+	$(this).toggleClass('active');
 });
 
 /* listen for date range picked to update the view and collapse the dropdown */
 $('#date-to').click( function () {
-    $('.report').toggleClass('hidden');
-    // $('.report').toggleClass('fadeout', function() {
-    // });
-    $('#dateRangePicker').collapse('hide');
+	$('.report').toggleClass('hidden');
+	// $('.report').toggleClass('fadeout', function() {
+	// });
+	$('#dateRangePicker').collapse('hide');
 });
 
 /* listen for PPG picked to update the view and collapse the dropdown */
 $('#ppg a').click( function () {
-    $('.report').toggleClass('hidden');
+	$('.report').toggleClass('hidden');
 });
 
 
 /* Swap tables on iframe click */
 window.addEventListener('blur',function(){
-    if(document.activeElement.id == 'iframe'){
-        $('.sidebar .insights').toggleClass('hidden');    
-    }
+	if(document.activeElement.id == 'iframe'){
+		$('.sidebar .insights').toggleClass('hidden');    
+	}
 });
 
 /* Animate scrolldown in trade promo page */
@@ -77,24 +89,24 @@ $('#carousel').carousel({
 
 /* fix promo calendar heights on open/close */
 $('.collapse').on('show.bs.collapse', function() {
-    $(this).closest('td').find('.progress').css({
-        'height':'120px',
-        'background-color':'#ffe600'
-    });
+	$(this).closest('td').find('.progress').css({
+		'height':'120px',
+		'background-color':'#ffe600'
+	});
 });
 
 $('.collapse').on('hide.bs.collapse', function() {
-    $(this).closest('td').find('.progress').css({
-        'height':'20px',
-        'background-color':'#337ab7'
-    });
+	$(this).closest('td').find('.progress').css({
+		'height':'20px',
+		'background-color':'#337ab7'
+	});
 });
 
 
 /* offcanvas.js */
 $(document).ready(function () {
   $('[data-toggle="offcanvas"]').click(function () {
-    $('.row-offcanvas').toggleClass('active')
+	$('.row-offcanvas').toggleClass('active')
   });
 });
 
